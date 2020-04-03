ec7133a90edae46c100a471c775dc40df4e08432
Aditya Singh <singh.aditya8499@gmail.com>
2020-04-03 13:33:25 +0530

    Created socket.io chat application

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ app.get('/',function(req,res){
 });
 
 io.on('connection',function(socket){
-    console.log('User is connected!');
-    socket.on('disconnect',function(socket){
-        console.log('User disconnected!');
+    // console.log('User is connected!');
+    socket.on('disconnect',function(){ //removed unused socket parameter from function
+        io.emit('disconnect user', 'User disconnected');
+    });
+
+    socket.on('chat message',function(msg){
+        io.emit('chat message', msg);
     });
 });
+
 
 http.listen(3000,function(){
     console.log('Listening on *:3000');
